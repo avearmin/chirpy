be5f508023eeb4f312f0dff4a0fab127310488e8
avearmin <arminradoncic1@gmail.com>
2023-09-18 23:25:00 -0400

    Add middleware to track number of fileserver hits

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
 
 func main() {
 	const root = "."
 	const appDir = "./app"
 	const port = "8080"
 
+	apiCfg := &apiConfig{
+		fileserverHits: 0,
+	}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(appDir))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(appDir)))))
 	mux.HandleFunc("/healthz", readinessEndpointHandler)
+	mux.HandleFunc("/metrics", apiCfg.fileServerHitsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
 	corsMux := middlewareCors(mux)
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -27,6 +38,25 @@ func readinessEndpointHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) fileServerHitsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
 }
 
 func middlewareCors(next http.Handler) http.Handler {
