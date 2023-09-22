4735d69c815eb66c58661b6dccffa7c5d13b93a7
avearmin <arminradoncic1@gmail.com>
2023-09-22 14:31:43 -0400

    Add reading/writing to/from gob to database package

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"encoding/gob"
 	"errors"
 	"io/fs"
 	"os"
@@ -8,8 +9,9 @@ import (
 )
 
 type DB struct {
-	path string
-	mux  *sync.RWMutex
+	path   string
+	mux    *sync.RWMutex
+	nextId int
 }
 
 type Chirp struct {
@@ -32,6 +34,35 @@ func NewDB(path string) (*DB, error) {
 	return &db, nil
 }
 
+func (db *DB) CreateChirp(body string) (Chirp, error) {
+	dbStruct, err := db.loadDB()
+	if err != nil {
+		return Chirp{}, err
+	}
+	chirp := Chirp{
+		Id:   db.nextId,
+		Body: body,
+	}
+	dbStruct.Chirps[db.nextId] = chirp
+	db.writeDB(dbStruct)
+	db.nextId++
+	return chirp, nil
+}
+
+func (db *DB) GetChirps() ([]Chirp, error) {
+	dbStruct, err := db.loadDB()
+	if err != nil {
+		return nil, err
+	}
+	keys := make([]Chirp, len(dbStruct.Chirps))
+	i := 0
+	for id := range dbStruct.Chirps {
+		keys[i] = dbStruct.Chirps[id]
+		i++
+	}
+	return keys, nil
+}
+
 func (db *DB) ensureDB() error {
 	if exists(db.path) {
 		return nil
@@ -40,6 +71,10 @@ func (db *DB) ensureDB() error {
 	if err != nil {
 		return err
 	}
+	dbStruct := DBStructure{
+		Chirps: make(map[int]Chirp),
+	}
+	db.writeDB(dbStruct)
 	return nil
 }
 
@@ -49,4 +84,35 @@ func exists(path string) bool {
 		return false
 	}
 	return true
+}
+
+func (db *DB) loadDB() (DBStructure, error) {
+	dbStruct := DBStructure{}
+	db.mux.RLocker().Lock()
+	defer db.mux.RLocker().Unlock()
+	file, err := os.Open(db.path)
+	if err != nil {
+		return DBStructure{}, err
+	}
+	defer file.Close()
+	decoder := gob.NewDecoder(file)
+	if err := decoder.Decode(&dbStruct); err != nil {
+		return DBStructure{}, err
+	}
+	return dbStruct, nil
+}
+
+func (db *DB) writeDB(dbStructure DBStructure) error {
+	db.mux.Lock()
+	defer db.mux.Unlock()
+	file, err := os.OpenFile(db.path, os.O_WRONLY|os.O_TRUNC, 0664)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	encoder := gob.NewEncoder(file)
+	if err := encoder.Encode(dbStructure); err != nil {
+		return err
+	}
+	return nil
 }
--- internal/database/database_test.go
+++ internal/database/database_test.go
@@ -1,10 +1,18 @@
 package database
 
-import "testing"
+import (
+	"os"
+	"sync"
+	"testing"
+)
 
 func Test(t *testing.T) {
 	runExistsTest(t, "./database.go", true)
 	runExistsTest(t, "./this/does/not/exists.go", false)
+
+	runEnsureDBTest(t)
+
+	runGetChirpsTest(t)
 }
 
 func runExistsTest(t *testing.T, path string, expecting bool) {
@@ -13,4 +21,70 @@ func runExistsTest(t *testing.T, path string, expecting bool) {
 	if got != expecting {
 		t.Errorf("Expecting: %t, but got: %t", expecting, got)
 	}
+}
+
+func runEnsureDBTest(t *testing.T) {
+	path := "./test_db.gob"
+	defer os.Remove(path)
+	db := &DB{
+		path:   path,
+		mux:    &sync.RWMutex{},
+		nextId: 0,
+	}
+	t.Logf("Starting test for ensureDB when DB does not exist with: \"%s\", and expecting: true", path)
+	err := db.ensureDB()
+	if err != nil {
+		t.Error(err)
+	}
+	got := exists(path)
+	if got != true {
+		t.Errorf("Expecting: true, but got: %t", got)
+	}
+	t.Logf("Starting test for ensureDB when DB does exist with: \"%s\", and expecting: true", path)
+	err = db.ensureDB()
+	if err != nil {
+		t.Error(err)
+	}
+	got = exists(path)
+	if got != true {
+		t.Errorf("Expecting: true, but got: %t", got)
+	}
+}
+
+func runGetChirpsTest(t *testing.T) {
+	path := "./test_db.gob"
+	defer os.Remove(path)
+
+	expecting := []Chirp{
+		{Id: 0, Body: "Some chirp"},
+		{Id: 1, Body: "Some other chirp"},
+	}
+
+	t.Logf("Starting test for ensureDB when DB does exist with: \"%s\", and expecting: %v", path, expecting)
+
+	db, err := NewDB(path)
+	if err != nil {
+		t.Error(err)
+	}
+
+	dbStruct := DBStructure{Chirps: make(map[int]Chirp)}
+	dbStruct.Chirps[0] = Chirp{Id: 0, Body: "Some chirp"}
+	dbStruct.Chirps[1] = Chirp{Id: 1, Body: "Some other chirp"}
+
+	err = db.writeDB(dbStruct)
+	if err != nil {
+		t.Error(err)
+	}
+
+	got, err := db.GetChirps()
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i, _ := range got {
+		if got[i] != expecting[i] {
+			t.Errorf("Expecting: %v, but got: %v", expecting, got)
+		}
+	}
+
 }
